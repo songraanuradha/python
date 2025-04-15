d6ee29d7be8d68fc944c68a9833954fa2fea5095
songraanuradha <songraanuradha@gmail.com>
2025-04-15 10:45:33 +0530

    Add files via upload

--- task_manager/task_project.py
+++ task_manager/task_project.py
@@ -1,5 +1,15 @@
 import sys
 import function as fun
+
+def getfun():
+        title = input("Enter your task title : ")
+        person = input("Enter your task person : ")
+        start_date = input("Enter date start_date : ")
+        end_date = input("Enter date end_date : ")
+        status = input("Enter your task status (0 = Pedding, 1 = Done) : ")
+        description = input("Enter your task description : ")
+        return title,person,start_date,end_date,status,description  
+
 def showTasks(sql,data=None):
        
         tables = fun.fetch(sql,data)
@@ -26,142 +36,139 @@ def showTasks(sql,data=None):
                 print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
                 
                 print(f"no of tasks is = {count}")
-        pause = input('press any key')
+        pause = input('press any key : ')
 
 
 while 1:
-        
-    print("Press 1 to view all task")
-    print("Press 2 to add new task")
-    print("Press 3 to update task")
-    print("Press 4 to delete task")
-    print("Press 5 to search task by title")
-    print("Press 6 to search task by person")
-    print("Press 7 to update task status")
-    print("Press 0 to exit")
-    try:
-        choice = int(input("Enter your choice : "))
-        if choice == 0:
-                    print("Good bye")
-                    break
-        elif choice == 1:
-                print('we will display all task')
+        try:    
+                print("Press 1 to view all task")
+                print("Press 2 to add new task")
+                print("Press 3 to update task")
+                print("Press 4 to delete task")
+                print("Press 5 to search task by title")
+                print("Press 6 to search task by person")
+                print("Press 7 to update task status")
+                print("Press 0 to exit")
                 
-                sql = "select * from tasks order by id "
-                showTasks(sql)
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")   
-                
-                     
-        elif choice == 2:
-                print('we will insert task')
-                title = input("Enter your task title : ")
-                person = input("Enter your task person : ")
-                start_date = input("Enter date start_date : ")
-                end_date = input("Enter date end_date : ")
-                status = input("Enter your task status (0 = Pedding, 1 = Done) : ")
-                description = input("Enter your task description : ")
-                pause = int(input("press any key : "))
-                
-                sql = "insert into tasks(title,person,start_date,end_date,status,description)values(%s,%s,%s,%s,%s,%s)"
-                values = [title,person,start_date,end_date,status,description]
-                count = fun.modify(sql,values)
-                
-                if count ==1:
-                        print("task inserted ")
-                else:
-                        print("task not inserted")        
-                
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-                
-                
-        elif choice == 3:
-                print('we will update task')
-                
-                sql1 = "select * from tasks order by id "
-                showTasks(sql1)
-                
-                id = int(input("Enter task id to update : ")) 
-                title = input("Enter your task title : ")
-                person = input("Enter your task person : ")
-                start_date = input("Enter date start_date : ")
-                end_date = input("Enter date end_date : ")
-                status = input("Enter your task status (0 = Pedding, 1 = Done) : ")
-                description = input("Enter your task description : ")
-                
-                sql2 = "update tasks set title=%s,person=%s,start_date=%s,end_date=%s,status=%s,description=%s where id=%s"
-                data = [title,person,start_date,end_date,status,description,id]
-                count = fun.modify(sql2,data)
-                
-                if count == 0:
-                        print('Task not found')
-                else:
-                        print('Task updated')
-
-                                
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-                
-                
-                
-        elif choice == 4:
-                print('we will delete task')
-                
-                sql = "select * from tasks order by id : "
-                showTasks(sql)
                 try:
-                        deletbyid = int(input("Enter id"))
-                        sql2 = "delete from tasks where id=%s"
-                        data=[deletbyid]
-                        count = fun.modify(sql2,data)
-                        if count==0:
-                           print('tasks not found')
-                        else: 
-                           print('tasks has been deleted successfully')
-                except ValueError:
-                        print("Invalid input. Please enter a numeric ID.")
-                
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                        choice = int(input("Enter your choice : "))
+                        if choice == 0:
+                                print("Good bye")
+                                break
+                        
+                        elif choice == 1:
                                 
-                
-        elif choice == 5:
-                print('we will search task by title : ')
-                
-                title = input("Enter your task title : ")
-                sql = "select * from tasks where title like %s"
-                data=[f"%{title}%"]
-                showTasks(sql,data)
-                
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-                
-                
-        elif choice == 6:
-                print('we will search task by person') 
-                person = input("Enter your task person : ")
-                sql = "select * from tasks where person like %s"
-                data=[f"%{person}%"]
-                showTasks(sql,data)
-                
-                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-                
-        elif choice == 7:
-                print('we will update task status')
-        
-                sql = "select * from tasks order by id"
-                showTasks(sql)
-                taskID = int(input("Enter task id to update status : ")) 
-                status = input("Enter book status 0 = Pedding, 1 = Done : ")
-                sql = "update tasks set status=%s where id=%s"
-                data = [status,taskID]
-                count = fun.modify(sql,data)
-                if count == 0:
-                   print('Task not found')
-                else:
-                   print('Task updated')
-                
-        else:
-                print('invalid choice')
-    except:
-        print("only number between (0 to 7) are allowed")
-        print("Oops!",sys.exc_info()[0],"occured.")
-        print()
+                                print('we will display all task')
+                                
+                                sql = "select * from tasks order by id "
+                                showTasks(sql)
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")   
+                                
+                                
+                        elif choice == 2:
+                                
+                                print('we will insert task')
+                                
+                                value = getfun()
+                                sql = "insert into tasks(title,person,start_date,end_date,status,description)values(%s,%s,%s,%s,%s,%s)"
+                                values = [value[0],value[1],value[2],value[3],value[4],value[5]]
+                                count = fun.modify(sql,values)
+                                if count ==1:
+                                        print("task inserted ")
+                                else:
+                                        print("task not inserted")
+                                pause = input('Enter any key : ')                
+                                
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                                
+                                
+                        elif choice == 3:
+                                
+                                print('we will update task')
+                                
+                                sql1 = "select * from tasks order by id "
+                                showTasks(sql1)
+                                id = int(input("Enter task id to update : ")) 
+                                value = getfun()
+                                sql2 = "update tasks set title=%s,person=%s,start_date=%s,end_date=%s,status=%s,description=%s where id=%s"
+                                data = [value[0],value[1],value[2],value[3],value[4],value[5],id]
+                                count = fun.modify(sql2,data)
+                                if count == 0:
+                                        print('Task not found')
+                                else:
+                                        print('Task updated')
+
+                                                
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                                
+                                
+                                
+                        elif choice == 4:
+                                
+                                print('we will delete task')
+                                
+                                sql = "select * from tasks order by id "
+                                showTasks(sql)
+                                try:
+                                        deletbyid = int(input("Enter id"))
+                                        sql2 = "delete from tasks where id=%s"
+                                        data=[deletbyid]
+                                        count = fun.modify(sql2,data)
+                                        if count==0:
+                                                print('tasks not found')
+                                        else: 
+                                                print('tasks has been deleted successfully')
+                                except ValueError:
+                                        print("Invalid input. Please enter a numeric ID.")
+                                
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                                                
+                                
+                        elif choice == 5:
+                                
+                                print('we will search task by title : ')
+                                
+                                title = input("Enter your task title : ")
+                                sql = "select * from tasks where title like %s"
+                                data=[f"%{title}%"]
+                                showTasks(sql,data)
+                                
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                                
+                                
+                        elif choice == 6:
+                                
+                                print('we will search task by person') 
+                                
+                                person = input("Enter your task person : ")
+                                sql = "select * from tasks where person like %s"
+                                data=[f"%{person}%"]
+                                showTasks(sql,data)
+                                
+                                print("--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+                                
+                        elif choice == 7:
+                                
+                                print('we will update task status')
+                        
+                                sql = "select * from tasks order by id"
+                                showTasks(sql)
+                                taskID = int(input("Enter task id to update status : ")) 
+                                status = input("Enter book status 0 = Pedding, 1 = Done : ")
+                                sql = "update tasks set status=%s where id=%s"
+                                data = [status,taskID]
+                                count = fun.modify(sql,data)
+                                if count == 0:
+                                        print('Task not found')
+                                else:
+                                        print('Task updated')
+                                
+                        else:
+                                print('invalid choice')
+                except:
+                        print("only number between (0 to 7) are allowed")
+                        print("Oops!",sys.exc_info()[0],"occured.")
+                        print()
             
-   
+        except:
+                print("invalid input, please enter a number") 
